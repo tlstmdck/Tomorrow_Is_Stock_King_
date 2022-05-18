ffb34bb896c3f34f7d8ad215ca1587664c64cbe3
JSI <suin8@jbnu.ac.kr>
2022-05-18 23:28:21 +0900

    feat: AddCommandInMainWindow
    - 새 게임 Command 추가
    - 종료 Command 추가

---
+++ Tomorrow_Is_Stock_King/ViewModel/Commands/ExitBtnCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Tomorrow_Is_Stock_King.ViewModel.Commands
+{
+    internal class ExitBtnCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Tomorrow_Is_Stock_King/ViewModel/Commands/NewGameBtnCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Tomorrow_Is_Stock_King.View.Windows;
+
+namespace Tomorrow_Is_Stock_King.ViewModel.Commands
+{
+    internal class NewGameBtnCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            StartSettingWindow startsettingwindow = new StartSettingWindow();
+            startsettingwindow.Show();
+            Application.Current.MainWindow.Close();
+            Application.Current.MainWindow = startsettingwindow;
+        }
+    }
+}
