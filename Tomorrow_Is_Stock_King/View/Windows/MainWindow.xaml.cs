527f21be0ee64c86e94668506a8b14f6ef43f8f7
JSI <suin8@jbnu.ac.kr>
2022-05-18 20:51:29 +0900

    style: file organization
    - 교수님이 하라는대로 폴더 정리
    - 폴더 경로 변경에 따른 파일별 경로 정리
    - 폰트적용

--- Tomorrow_Is_Stock_King/View/Windows/MainWindow.xaml.cs
+++ Tomorrow_Is_Stock_King/View/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tomorrow_Is_Stock_King.Windows;
 
 namespace Tomorrow_Is_Stock_King
 {
