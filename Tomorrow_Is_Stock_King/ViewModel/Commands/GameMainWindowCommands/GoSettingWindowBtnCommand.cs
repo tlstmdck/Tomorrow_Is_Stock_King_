f542a2d9c6b824fe4883927bc261e5244cbfc159
JSI <suin8@jbnu.ac.kr>
2022-05-19 15:27:42 +0900

    feat: AddGameMainWindowCommand
    GameMainWIndow에서 설정버튼 클릭시
    SettingWindow 나타나는 command구현

---
+++ Tomorrow_Is_Stock_King/ViewModel/Commands/GameMainWindowCommands/GoSettingWindowBtnCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Tomorrow_Is_Stock_King.View.Windows;
+
+namespace Tomorrow_Is_Stock_King.ViewModel.Commands.GameMainWindowCommands
+{
+    internal class GoSettingWindowBtnCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            SettingWindow settingwindow = new SettingWindow();
+            settingwindow.ShowDialog();
+            Application.Current.MainWindow = settingwindow;
+        }
+    }
+}
