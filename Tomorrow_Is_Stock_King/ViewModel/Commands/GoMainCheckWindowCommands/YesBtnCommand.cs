a73837fd31e7848758ad656758265ebcb45527fc
JSI <suin8@jbnu.ac.kr>
2022-05-19 19:48:39 +0900

    feat: AddGoMainCheckWindowCommands
    GoMainCheckWindowCommand들
    추가하였습니다.
    
    [#7]

---
+++ Tomorrow_Is_Stock_King/ViewModel/Commands/GoMainCheckWindowCommands/YesBtnCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Tomorrow_Is_Stock_King.Windows;
+
+namespace Tomorrow_Is_Stock_King.ViewModel.Commands.GoMainCheckWindowCommands
+{
+    internal class YesBtnCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.Show();
+            Application.Current.MainWindow = mainwindow;
+        }
+    }
+}
