6516e447a8ac291ad30ca5bf8d9f0e496779df4e
JSI <suin8@jbnu.ac.kr>
2022-06-14 16:27:05 +0900

    feat: Add HelpPage
    도움페이지 틀 제작

---
+++ Tomorrow_Is_Stock_King/ViewModel/Commands/ShowHelpWindowCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Tomorrow_Is_Stock_King.View.Windows;
+
+namespace Tomorrow_Is_Stock_King.ViewModel.Commands
+{
+    public class ShowHelpWindowCommand : ICommand
+    {
+        SoundVM SoundVM { get; set; }
+        public ShowHelpWindowCommand(SoundVM vm)
+        {
+            SoundVM = vm;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (SoundVM.SoundDataToShow.IsTurnOnEffect)
+            {
+                SoundVM.playClickSound();
+            }
+
+            ShowHelpWindow showhelpwindow = new ShowHelpWindow();
+            showhelpwindow.ShowDialog();
+        }
+    }
+}
